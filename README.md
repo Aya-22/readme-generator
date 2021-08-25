451a0dc454508b286ba19e3a85dd3ae5139c7aa5
Jennifer Ayala <j.ayala0695@gmail.com>
2021-08-24 23:08:17 -0700

    updated video link to readme

--- README.md
+++ README.md
@@ -1,30 +1,34 @@
 # readme-generator       
   
   ## Description
-  
+  The objective was to create a readme using the starter code.
   
   
   ## Table of Contents (Optional)
-  
+  1. index.js 2. generateMarkdown.js
  
   ## Installation
-  
+  1. Install npm 
   
   ## Usage
   
   
   ## Credits
-  
+  Jennifer
  
   ## License
-  Boost Software License 1.0
+  Apache 2.0 License
+  (https://opensource.org/licenses/Apache-2.0)
  
   ## Badges
-  [![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)]
+  [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]
 
   ## Features
   
   ## How to Contribute
   
   ## Tests
+
+  To see a walkthrough of my code click on the link below:
+  https://youtu.be/q1k_IY3NCD4
   
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,58 +1,59 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  const apacheLicense = 'Apache 2.0 License';
-  const boostLicense = 'Boost Software License 1.0';
-  const mitLicense = 'MIT';
-    if (license === apacheLicense) {
-   return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]`;
-    } if (license === boostLicense) {
-   return `[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)]`;
-    } if (license === mitLicense) {
-   return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`;  
-    } else {
-      return 'No license was used.'
-    }
-  
-
+  const apacheLicense = "Apache 2.0 License";
+  const boostLicense = "Boost Software License 1.0";
+  const mitLicense = "MIT";
+  if (license === apacheLicense) {
+    return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]`;
+  }
+  if (license === boostLicense) {
+    return `[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)]`;
+  }
+  if (license === mitLicense) {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`;
+  } else {
+    return "No license was used.";
+  }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  const apacheLicense = 'Apache 2.0 License';
-  const boostLicense = 'Boost Software License 1.0';
-  const mitLicense = 'MIT';
-    if (license === apacheLicense) {
-   return `(https://opensource.org/licenses/Apache-2.0)`;
-    } if (license === boostLicense) {
-   return `(https://www.boost.org/LICENSE_1_0.txt)`;
-    } if (license === mitLicense) {
-   return `(https://opensource.org/licenses/MIT)`;  
-    } else {
-      return 'No license was used.'
-    }
-
+  const apacheLicense = "Apache 2.0 License";
+  const boostLicense = "Boost Software License 1.0";
+  const mitLicense = "MIT";
+  if (license === apacheLicense) {
+    return `(https://opensource.org/licenses/Apache-2.0)`;
+  }
+  if (license === boostLicense) {
+    return `(https://www.boost.org/LICENSE_1_0.txt)`;
+  }
+  if (license === mitLicense) {
+    return `(https://opensource.org/licenses/MIT)`;
+  } else {
+    return "No license was used.";
+  }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  const apacheLicense = 'Apache 2.0 License';
-  const boostLicense = 'Boost Software License 1.0';
-  const mitLicense = 'MIT';
-    if (license === apacheLicense) {
-   return `Apache License
+  const apacheLicense = "Apache 2.0 License";
+  const boostLicense = "Boost Software License 1.0";
+  const mitLicense = "MIT";
+  if (license === apacheLicense) {
+    return `Apache License
    Version 2.0, January 2004
 http://www.apache.org/licenses/`;
-  //   } if (license === boostLicense) {
-  //  return `(https://www.boost.org/LICENSE_1_0.txt)`;
-  //   } if (license === mitLicense) {
-  //  return `(https://opensource.org/licenses/MIT)`;  
-  //   } else {
-  //     return 'No license was used.'
-  //   }
-
+    //   } if (license === boostLicense) {
+    //  return `(https://www.boost.org/LICENSE_1_0.txt)`;
+    //   } if (license === mitLicense) {
+    //  return `(https://opensource.org/licenses/MIT)`;
+    //   } else {
+    //     return 'No license was used.'
+    //   }
+  }
 }
 
 // TODO: Create a function to generate markdown for README
@@ -87,6 +88,9 @@ function generateMarkdown(data) {
   ## How to Contribute
   
   ## Tests
+
+  To see a walkthrough of my code click on the link below:
+  https://youtu.be/q1k_IY3NCD4
   `;
 }
 
